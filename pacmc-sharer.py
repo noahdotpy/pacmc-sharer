1e3c993e3c0875bb1d45303e79f08c814f10be0b
noahdotpy <noah.slykerman@gmail.com>
2022-07-11 17:15:50 +1000

    Updated --help messages

--- pacmc-sharer.py
+++ pacmc-sharer.py
@@ -13,11 +13,11 @@ def main():
     group.add_argument('--game-version', '-gv', action='store', default=[''], nargs=1, metavar=(
         '<Game version>',), help='Which version of minecraft is the archive for?', required=True)
     group.add_argument('--short', '-s', action='store_true', default=False,
-                       help='Used to get a short, one-line version of the normal script code.')
+                       help='Used to get a short, one-line version of the normal script code (easier to copy/paste).')
     group.add_argument('--no-file', '-nf', action='store_true',
-                       default=False, help='Use this to not get a file.')
+                       default=False, help='Use this if you don\'t want a file to be created.')
     group.add_argument('--copy', '-c', action='store_true',
-                       default=False, help='Use this copy the final script.')
+                       default=False, help='Use this to copy the script to system clipboard.')
     args = parser.parse_args()
     create_script(args.archive_path,
                   args.file[0], args.game_version[0], args.short, args.no_file, args.copy)
