d6fc176f21290d52b54bdb61840e023ccb857071
noahdotpy <noah.slykerman@gmail.com>
2022-07-24 20:42:35 +1000

    Edited arg help messages to be more concise

--- pacmc-sharer.py
+++ pacmc-sharer.py
@@ -4,19 +4,20 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser(
-        description="Scans your specified folder for mods downloaded with the PacMC package manager and outputs all valid PacMC mods in the folder.")
+        description="Scans your specified folder for mods downloaded with the PacMC package manager and outputs all valid PacMC mods in the folder."
+    )
     group = parser.add_argument_group()
     
     group.add_argument('archive_path', help='Path to the archive you are pulling mods list from.')
     
     group.add_argument('--file', '-f', action='store', default=[''], nargs=1, metavar=(
-        '<file_path>',), help='Path to the output file (if none specified, no file)')
+        '<file_path>',), help='Write mod list to a file.')
     
-    group.add_argument('--copy', '-c', action='store_true',
-                       default=False, help='Use this to copy the mod list to the system clipboard.')
+    group.add_argument('--copy', '-c', action='store_true', default=False,
+                       help='Copy mod list to clipboard.')
     
-    group.add_argument('--include-repo', action='store_true',
-                       default=False, help='Use this to implement the repo the mod came from into the output.')
+    group.add_argument('--include-repo', action='store_true', default=False,
+                       help='Puts the repo into the mod slug (modrinth/sodium, curseforge/sodium).')
     
     py_args = parser.parse_args()
     py_args.file = py_args.file[0]
