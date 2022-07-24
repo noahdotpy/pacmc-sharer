a2c7b140eb14daa94abadd4413a3829e262bd2a7
noahdotpy <noah.slykerman@gmail.com>
2022-07-24 20:32:26 +1000

    Simplified the whole program and refactored - Program now only lists out all of the mods as that is the only needed functionality - Cleaned up whole codebase

--- functions.py
+++ functions.py
@@ -3,46 +3,13 @@ from os.path import isfile, join
 
 import pyperclip
 
-def render_script(short: bool, game_version: str, valid_files: dict):
-    if short:
-            return f"""echo -n "Where do you want this new archive? "; read ARC_DIR && echo -n "What do you want your archive to be called? "; read ARC_NAME; echo -n "What repo do you want to use? (modrinth, curseforge, ENTER to choose for each mod) "; read REPO; if [ "$REPO" = "" ]; then echo "Repo: None"; else REPO+="/"; echo "Repo: $REPO"; fi; GAME_VERSION=\'{game_version}\'; pacmc archive create $ARC_NAME $ARC_DIR; pacmc archive version $ARC_NAME --game-version $GAME_VERSION; """r"pacmc install -y -a $ARC_NAME " + ' '.join([valid_files[mod_id]["repo"]+ valid_files[mod_id]["name"] for mod_id in valid_files])
-    else:
-        return f"""#!/bin/bash      
-            
-# !! IMPORTANT: YOU NEED TO SET EXECUTABLE PERMS FOR THIS SCRIPT BEFORE RUNNING !!
-
-
-######## CONFIG ########
-
-
-# => Archive which the mods will be downloaded to.
-
-# Path to the archive that will be created
-echo -n "Where do you want this new archive? "
-read ARC_DIR # assign input value into a variable
-
-# Unique name/identifier for the archive that will be created
-echo -n "What do you want your archive to be called? "
-read ARC_NAME # assign input value into a variable
-
-# => Version of Minecraft (RECOMMENDED NOT TO CHANGE THIS SETTING)
-GAME_VERSION=\'{game_version}\'
-
-######## CREATE ARCHIVE AND INSTALL MODS ########
-
-# Creating archive
-pacmc archive create $ARC_NAME $ARC_DIR
-pacmc archive version $ARC_NAME --game-version $GAME_VERSION
-
-# All the individual install scripts
-""" + r"""pacmc install -y -a $ARC_NAME """ + ' '.join([valid_files[mod_id]["repo"] + valid_files[mod_id]["name"] for mod_id in valid_files])
-
-def create_script(archive_path: str, out_file_path: str, game_version: str, short: bool, no_file: bool, copy: bool):
-    valid_files = {}
-    files = [f for f in listdir(archive_path) if isfile(join(archive_path, f))]
+def create_script(py_args):
+    mods = []
+    files = [f for f in listdir(py_args.archive_path) if isfile(join(py_args.archive_path, f))]
     
-    # Checking for valid files and adding them to valid_files if True
-    for index, file in enumerate(files):
+    # Checking for valid files and adding them to mods list if True
+    for file in files:
+        # check if the .jar is a valid pacmc file
         if file.endswith('.pacmc.jar'):
             # modrinth
             if '_mr_' in file:
@@ -52,19 +19,18 @@ def create_script(archive_path: str, out_file_path: str, game_version: str, shor
             if '_cf_' in file:
                 splitted = file.split('_cf_')
                 mod_repo = "curseforge/"
-            mod_name = splitted[0]
-            mod_id = splitted[1].split('.pacmc.jar')[0]
-            valid_files[mod_id] = {
-                'name': mod_name,
-                'repo': mod_repo
-            }
+            if not py_args.include_repo:
+                mod_repo = ""
+            mod_slug = splitted[0]
+            mods.append(f"{mod_repo}{mod_slug}")
 
     # Render the output shell script
-    if not no_file:
-        with open(out_file_path, 'w') as out_file:
-            print(render_script(short, game_version, valid_files), file=out_file)
-            print("INFO: Created file at " + out_file_path)
+    mods_list_as_str = " ".join([mod for mod in mods])
+    if py_args.file:
+        with open(py_args.file, 'w') as out_file:
+            print(mods_list_as_str , file=out_file)
+            print("INFO: Created file at " + py_args.file)
 
-    if copy:
-        pyperclip.copy(render_script(short, game_version, valid_files))
-        print("INFO: Script copied to clipboard!")
+    if py_args.copy:
+        pyperclip.copy(mods_list_as_str )
+        print("INFO: Mod list copied to clipboard!")
--- pacmc-sharer.py
+++ pacmc-sharer.py
@@ -6,21 +6,21 @@ def main():
     parser = argparse.ArgumentParser(
         description="A Simple Pacmc Archive Sharer.")
     group = parser.add_argument_group()
-    group.add_argument(
-        'archive_path', help='Path to the archive you want to share.')
-    group.add_argument('--file', '-f', action='store', default=['output.sh'], nargs=1, metavar=(
-        '<File name>',), help='Where do you want the file to be created?')
-    group.add_argument('--game-version', '-gv', action='store', default=[''], nargs=1, metavar=(
-        '<Game version>',), help='Which version of minecraft is the archive for?', required=True)
-    group.add_argument('--short', '-s', action='store_true', default=False,
-                       help='Used to get a short, one-line version of the normal script code (easier to copy/paste).')
-    group.add_argument('--no-file', '-nf', action='store_true',
-                       default=False, help='Use this if you don\'t want a file to be created.')
+    
+    group.add_argument('archive_path', help='Path to the archive you are pulling mods list from.')
+    
+    group.add_argument('--file', '-f', action='store', default=[''], nargs=1, metavar=(
+        '<file_path>',), help='Path to the output file (if none specified, no file)')
+    
     group.add_argument('--copy', '-c', action='store_true',
-                       default=False, help='Use this to copy the script to system clipboard.')
-    args = parser.parse_args()
-    create_script(args.archive_path,
-                  args.file[0], args.game_version[0], args.short, args.no_file, args.copy)
+                       default=False, help='Use this to copy the mod list to the system clipboard.')
+    
+    group.add_argument('--include-repo', action='store_true',
+                       default=False, help='Use this to implement the repo the mod came from into the output.')
+    
+    py_args = parser.parse_args()
+    py_args.file = py_args.file[0]
+    create_script(py_args)
 
 
 if __name__ == '__main__':
